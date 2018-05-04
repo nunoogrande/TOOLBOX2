2a81f593ebc33cf0538eebbd5927e9eca7284abb
nunoogrande <35113137+nunoogrande@users.noreply.github.com>
2018-05-04 19:17:34 +0100

    Loja (fichas )

---
+++ loja/components/TheHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <header class="main-header">
+      <div>
+        <nav class="main-nav">
+          <ul class="nav-links">
+            <nuxt-link to="/" tag="li" class="nav-link"><a>ALL Posts</a></nuxt-link>
+            <nuxt-link to="/about" tag="li" class="nav-link"><a>About</a></nuxt-link>
+          </ul>
+        </nav>
+      </div>
+      <SideNavToggle @toggle="$emit('SideNavToggle')"/>
+    </header>
+  </div>
+</template>
+
+<script>
+import SideNavToggle from "@/components/SideNavToggle";
+
+export default {
+  name:"TheHeader",
+  components:{
+    SideNavToggle
+  }
+}
+</script>
+
+
+<style scoped>
+  .main-header{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background: #022d30;
+    height: 4.5rem;
+  }
+  .main-nav{
+    height: 100%;
+  }
+  .nav-links{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+  .nav-link{
+    height: 100%;
+    margin: 0.1rem;
+    padding: 0.3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .nav-link a{
+    display: block;
+    text-decoration: none;
+    color: white;
+  }
+  .nav-link a:hover,.nav-link a:active,.nav-link.nuxt-link-exact-active a{
+    color: #06c4d1;
+  }
+  .nav-link.nuxt-link-exact-active{
+    border-bottom: 3px solid #06c4d1;
+  }
+</style>
---
+++ loja/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <TheHeader @SideNavToggle="displaySideNave = !displaySideNave"/>
+    <SideNav :show="displaySideNave" @close="displaySideNave=false"/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader"
+import SideNav from "@/components/SideNav"
+
+export default {
+  components:{
+    TheHeader,
+    SideNav
+  },
+  data(){
+    return{
+      displaySideNave:false
+    }
+  }
+}
+</script>
+
+
+<style>
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    margin-top: 5rem;
+    font-family: "lato", sans-serif;
+  }
+  .main-content{
+    margin-top: 4.5rem;
+  }
+
+</style>
